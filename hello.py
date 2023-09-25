8f4714d44e4d96d9f63c7c0caaa9da41ca972023
Himanish Jindal <jhimanish@gmail.com>
2023-09-24 21:33:47 -0400

    Adding UofT Email address to form and error testing for email

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hard to guess string"
@@ -14,19 +14,34 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField(
+        "What is your UofT Email address?", validators=[DataRequired(), Email()]
+    )
     submit = SubmitField("Submit")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    email_val = False
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get("name")
         if old_name is not None and old_name != form.name.data:
             flash("Looks like you have changed your name!")
+        if "utoronto" in form.email.data:
+            email_val = True
+        else:
+            flash("Please use your UofT Email")
         session["name"] = form.name.data
+        session["email"] = form.email.data
         return redirect(url_for("index"))
-    return render_template("index.html", form=form, name=session.get("name"))
+    return render_template(
+        "index.html",
+        form=form,
+        name=session.get("name"),
+        email=session.get("email"),
+        email_val=email_val,
+    )
 
 
 @app.route("/user/<name>")
